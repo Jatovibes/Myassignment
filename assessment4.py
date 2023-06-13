8a151e61e55231b4bd3418297d163ea6b5edd88c
Jatovibes <Jatoboi123@gmail.com>
2023-06-13 10:24:20 +0100

    assessment

--- assessment4.py
+++ assessment4.py
@@ -1,8 +1,5 @@
 def reverse_string(input_string):
-    output_string = ""
-    for i in range(len(input_string)-1, -1, -1):
-        output_string += input_string[i]
-    return output_string
+  return input_string[::-1]
 input_string = "My name is Josh"
 output_string = reverse_string(input_string)
 print("The reversed string is:", output_string)
